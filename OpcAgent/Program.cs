221f013be7316cac83b5d991f5f784e409e92167
ulaneb <ul0275817@edu.uni.lodz.pl>
2024-12-21 22:44:54 +0100

    Add project files.

---
+++ OpcAgent/Program.cs
@@ -0,0 +1,31 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+
+using (var client = new OpcClient("opc.tcp://localhost:4840/"))
+{
+    client.Connect();
+
+    OpcReadNode[] commands = new OpcReadNode[] {
+    new OpcReadNode("ns=2;s=Device 1/ProductionStatus", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/ProductionStatus"),
+    new OpcReadNode("ns=2;s=Device 1/ProductionRate", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/ProductionRate"),
+    new OpcReadNode("ns=2;s=Device 1/WorkorderId", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/WorkorderId"),
+    new OpcReadNode("ns=2;s=Device 1/Temperature", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/Temperature"),
+    new OpcReadNode("ns=2;s=Device 1/GoodCount", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/GoodCount"),
+    new OpcReadNode("ns=2;s=Device 1/BadCount", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/BadCount"),
+    new OpcReadNode("ns=2;s=Device 1/DeviceError", OpcAttribute.DisplayName),
+    new OpcReadNode("ns=2;s=Device 1/DeviceError"),
+};
+
+    IEnumerable<OpcValue> job = client.ReadNodes(commands);
+
+    foreach (var item in job)
+    {
+        Console.WriteLine(item.Value);
+    }
+}
